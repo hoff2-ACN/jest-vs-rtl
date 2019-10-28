ac5a9146c0259eee21f58218234e3c31abe3855c
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-28 14:17:59 -0500

    Converted App from class to functional style

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text testID="welcome" style={styles.welcome}>Hello!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+const App = () => (
       <View style={styles.container}>
         <Text testID="welcome" style={styles.welcome}>Hello!</Text>
       </View>
-    );
-  }
-}
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
