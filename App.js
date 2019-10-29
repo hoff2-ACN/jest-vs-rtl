48d17e940b106eb2f51a7aaca1a13b907ab3956c
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-29 10:08:22 -0500

    Added functioning detox test and tooling support for android

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text testID="welcome" style={styles.welcome}>Hello!</Text>
-//       </View>
-//     );
-//   }
-// }
 
 const App = () => (
       <View style={styles.container}>
